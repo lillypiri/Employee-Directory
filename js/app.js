65b28eaed00926b21e217daf1159403cda90137a
Lilly Piri <lilly@lillypiri.com>
2017-09-29 09:14:59 +1000

    refactored everything. next and previous now work.

--- js/app.js
+++ js/app.js
@@ -1,90 +1,124 @@
-var xhr = new XMLHttpRequest();
+(function() {
+    var modal = document.getElementById('myModal');
+    var modalContent = document.querySelector('.modal-content');
+    var usercard = document.querySelectorAll('.item');
 
-var modal = document.getElementById('myModal');
-var span = document.querySelector('.close');
-var modalContent = document.querySelector('.modal-content');
+    var URL = "https://randomuser.me/api/?results=12";
 
-xhr.onload = function() {
-    if (xhr.readyState === xhr.DONE) {
-        var data = xhr.response;
-        var jsonResponse = JSON.parse(data);
+    var state = {
+        users: [],
+        isModal: false,
+        modalUserIndex: 0
+    };
 
-        jsonResponse.results.forEach(function(user, index) {
-            var d = user.dob.slice(0, 10).split('-');
-            var item = document.querySelector(".item-" + (index + 1));
-            item.setAttribute("data-index", index);
-            item.addEventListener("click",function (event) {
-                // console.log("modal content", modalContent);
-                modalContent.innerHTML =
-                '<div>\
-                <span id="close" class="close">' + '&times' + '</span>\
-                <img src="' + user.picture.large + '" class ="profile-picture-modal">\
-                </div>\
-                <div class="user-card-modal">\
-                    <strong>' + user.name.first + " " + user.name.last + '</strong>\
-                    <div class="email">' + user.email + '</div>\
-                    <div>' + user.location.city + '</div>\
-                    <hr/>\
-                    <div>' + user.phone + '</div>\
-                    <div>' + user.location.street + ", " + user.location.city + ", " + user.location.postcode + '</div>\
-                    <div>' + "Birthday: " + d[1] +'/'+ d[2] +'/'+ d[0] + '</div>\
-                    <hr/>\
-                    <div>' + "⇇ previous     |" + "     next ⇉" + '</div>\
-                </div>'
-                item.getAttribute("data-index");
-                showModal();
-            });
-            item.innerHTML = '\
-                <div class="user-card">\
-                    <img src="'+ user.picture.medium + '" class="profile-picture">\
-                    <div>\
-                        <strong>' + user.name.first + " " + user.name.last + '</strong>\
-                        <div class="email">' + user.email + '</div>\
-                        <div>' + user.location.city + '</div>\
-                    </div>\
-                </div>'
+    // Load data from a URL and then pass it to the callback
+    function loadUrl(url, callback) {
+        var xhr = new XMLHttpRequest();
 
+        xhr.onload = function() {
+            if (xhr.readyState === xhr.DONE) {
+                var jsonResponse = JSON.parse(xhr.response).results;
+                callback(jsonResponse);
+            } else {
+                console.log("Not ready yet.");
+            }
+        };
 
-        });
-    } else {
-        console.log("Not ready yet.");
+        xhr.open("GET", url);
+        xhr.send();
     }
-};
 
-xhr.open("GET", "https://randomuser.me/api/?results=12");
+    // Only renders stuff
+    function render() {
+        if (state.isModal) {
+            document.body.style.overflow = 'hidden';
 
-xhr.send();
+            var user = state.users[state.modalUserIndex];
 
-var usercard = document.querySelectorAll('.item');
+            // Make the date of birth human readable
+            var d = user.dob.slice(0, 10).split('-');
 
-function showModal() {
-    var data = xhr.response;
-    var jsonResponse = JSON.parse(data);
-    modal.style.display = "block";
-    span = document.querySelector('.close');
-    closeModal();
-};
+            modalContent.innerHTML =
+                `<div>
+                <span id="close" class="close">&times</span>
+                <img src="${user.picture.large}" class="profile-picture-modal">
+                </div>
+                <div class="user-card-modal">
+                    <strong>${user.name.first} ${user.name.last}</strong>
+                    <div>${user.login.username}</div>
+                    <div class="email">${user.email}</div>
+                    <hr/>
+                    <div>${user.phone}</div>
+                    <div>${user.location.street}, ${user.location.city}, ${user.location.postcode}, ${user.nat}</div>\
+                    <div>Birthday:  ${d[1]}/${d[2]}/${d[0]}</div>
+                    <hr/>
+                    <div><span class="previous">&larr; previous     |</span>
+                    <span class="next">next &rarr;</div>
+                </div>`
+            // Close the modal
+            modalContent.querySelector('.close').addEventListener('click', function(event) {
+                state.isModal = false;
+                render();
+            });
 
-modal.onclick = function(event) {
-    //getNextUser();
-}
+            // Previous user 
+            modalContent.querySelector('.previous').addEventListener('click', function(event) {
+                if (state.modalUserIndex === 0) {
+                    state.modalUserIndex = state.users.length - 1;
+                } else {
+                    state.modalUserIndex --;
+                }
+                render();
+            });
 
-function closeModal() {
-    span.onclick = function(event) {
-        if (event.target == span) {
+            // Next user
+            modalContent.querySelector('.next').addEventListener('click', function(event) {
+                if (state.modalUserIndex === state.users.length - 1) {
+                    state.modalUserIndex = 0;
+                } else {
+                    state.modalUserIndex ++;
+                }
+                render();
+            });
+
+            modal.style.display = "block";
+        } else {
+            document.body.style.overflow = '';
             modal.style.display = "none";
         }
     }
-};
 
-window.onclick = function(event) {
-    console.log(event.target);
-    if (event.target == modal) {
-        modal.style.display = "none";
+
+    function init() {
+        // Load the users
+        loadUrl(URL, function(users) {
+            // Save the list of users for rendering
+            state.users = users;
+
+            // Build the DOM
+            users.forEach(function(user, index) {
+                var item = document.querySelector(".item-" + (index + 1));
+                item.setAttribute("data-index", index);
+                item.innerHTML =
+                    `<div class="user-card">
+                        <img src="${user.picture.medium}" class="profile-picture">
+                        <div>
+                            <strong>${user.name.first} ${user.name.last}</strong>
+                            <div class="email">${user.email}</div>
+                            <div>${user.location.city}</div>
+                        </div>
+                    </div>`;
+                item.addEventListener('click', function (event) {
+                    state.isModal = true;
+                    state.modalUserIndex = index;
+                    render();
+                });
+            });
+        });
     }
-}
 
-//
-// function getNextUser() {
-//
-// }
+
+
+
+    init();
+})();
